82e478aa6ac9e2df191863c8cda1f22647a04d21
Joel Catarino <62433408+JoelCatarino@users.noreply.github.com>
2021-07-09 16:19:42 -0300

    first_commit

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/icons/util-area.png">
+    <title>Sua Imobiliáira</title>
+
+    <link rel="stylesheet" href="cdn/css/bootstrap_custom.css">
+    <link rel="stylesheet" href="cdn/css/app.css">
+</head>
+
+<body>
+
+    <header class="main_header">
+
+        <div class="header_bar bg-front">
+
+            <div class="conteiner">
+                <div class="row">
+                    <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                        <i class="icon-location-arrow"></i>
+                        <p class="my-auto mx-3">Imbassai, 0 <br /> Mata de São João-BA</p>
+                    </div>
+
+                    <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                        <i class="icon-clock-o"></i>
+                        <p class="my-auto mx-3">Seg/Sex 09:00h - 18:00h<br />Sáb/Dom Plantão</p>
+                    </div>
+
+                    <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                        <i class="icon-envelope"></i>
+                        <p class="my-auto mx-3">joelscatarino@outlook.com<br />+55 (71)99738-5375</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light my-3">
+
+            <div class="container">
+
+                <div class="navbar-brand">
+                    <a href="index.php">
+                        <img class="d-inline-block" width="280" src="assets/images/logo.png">
+                    </a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggle-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link active text-front" aria-current="page" href="#">Destaque</a></li>
+                        <li class="nav-item"> <a class="nav-link active" aria-current="page" href="#">Alugar</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Comprar</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Contato</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </nav>
+
+    </header>
+
+    <?php
+    $getApp = filter_input(INPUT_GET, 'app', FILTER_SANITIZE_STRIPPED);
+
+    if (empty($getApp)) {
+        require 'widget/home.php';
+    } elseif (!empty($getApp) && file_exists('widget/' . $getApp . '.php')) {
+        require 'widget/' . $getApp . '.php';
+    } else {
+        echo "Oooops, erro no app informado!";
+    }
+    ?>
+
+    <article class="main_optin bg-dark text-white py-5">
+        <div class="container">
+            <div class="row mx-auto" style="max-width: 600px;">
+                <h1 class="bceta mb-3">Quer ficar por dentro das novidades?</h1>
+
+                <p>Deixe o seu melhor e-mail nos campos abaixo e nós vamos lhe <br> informar sobre os melhores negócios e todos os lancamentos do sul da ilha</p>
+
+                <form action="">
+                    <input type="text" class="form-control" placeholder="Digite seu nome" size="50">
+                    <input type="email" class="form-control" placeholder="Digite seu e-mail" size="50">
+                    <button type="submit" class="btn btn-front">Me avise!</button>
+                </form>
+            </div>
+        </div>
+    </article>
+
+    <section class="main_footer bg-light" style="background: url(assets/images/footer.png) repeat-x bottom center; background-size: 10%">
+        <div class="container pt-5" style="padding-bottom: 120px;">
+            <div class="row justify-content-around text-muted">
+                <div class="col-3">
+                    <h1 class="pb-2">Navegue <span class="text-front">Aqui</span></h1>
+                    <ul>
+                        <li><a href="">home</a></li>
+                        <li><a href="" class="text-front">Destaque</a></li>
+                        <li><a href="">Alugar</a></li>
+                        <li><a href="">Comprar</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-6">
+                    <h1 class="pd2">Nos <span class="text-front">Conheca! </span></h1>
+                    <p>Nossa maior satisfacao é lhe ajudar a encontrar seu imóvel dos sonhos nos bairros do Sul da Ilha da Mariga, em Florianópolis.</p>
+
+                    <h1 class="pb-2">Quer <span class="text-front">Investir</span></h1>
+                </div>
+
+                <div class="col-3">
+                    <button class="btn btn-front icon-facebook icon-notext"></button>
+                    <button class="btn btn-front icon-twitter icon-notext"></button>
+                    <button class="btn btn-front icon-instagram icon-notext"></button>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <div class="main_copyright py-3 bg-front text-white text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                <p class="mb-0">Imobiliária | CRECI 1234 | Av. Pequeno Principe, 0 Campeche - Florianópolis/SC</p>
+                <p class="mb-0">Todos os Direitos Reservados - Up Inside Treinamentos @</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="node_modules/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="node_modules/bootstrap-select/dist/js/i18n/defaults-pt_BR.min.js"></script>
+</body>
+
+</html>
---
+++ widget/home.php
@@ -0,0 +1,438 @@
+<div class="main_slide">
+    <div class="container" style="height: 100%;">
+        <div class="row align-items-center" style="height: 100%;">
+            <!-- style="height: 100%;"  -->
+            <div class="col-8">
+                <p class="main_slide_content">Encontre o <b>imóvel ideal</b> <br> para você e <b>sua família</b><br>morar na praia!</p>
+                <a href="#" class="btn btn-front btn-lg">Quero alugar</a>
+                <a href="#" class="btn btn-front btn-lg">Quero alugar</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="main_filter">
+    <div class="container my-5">
+        <div class="row">
+            <form action="" class="form_inline w-100 d-flex">
+
+                <div class="form-group col-3">
+                    <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                    <select name="search" id="search" class="selectpicker" title="Escolha...">
+                        <option value="">Comprar</option>
+                        <option value="">Alugar</option>
+                    </select>
+                </div>
+
+                <div class="form-group col-3">
+                    <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                    <select name="search" id="search" class="selectpicker" title="Escolha...">
+                        <option value="">Comprar</option>
+                        <option value="">Alugar</option>
+                    </select>
+                </div>
+
+                <div class="form-group col-3">
+                    <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                    <select name="search" id="search" class="selectpicker" title="Escolha...">
+                        <option value="">Comprar</option>
+                        <option value="">Alugar</option>
+                    </select>
+                </div>
+
+                <div class="form-group col-3">
+                    <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                    <select name="search" id="search" class="selectpicker" title="Escolha...">
+                        <option value="">Comprar</option>
+                        <option value="">Alugar</option>
+                    </select>
+                </div>
+
+                <div style="display: none;">
+
+                    <div class="form-group col-3">
+                        <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                        <select name="search" id="search" class="selectpicker" title="Escolha...">
+                            <option value="">Comprar</option>
+                            <option value="">Alugar</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-3">
+                        <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                        <select name="search" id="search" class="selectpicker" title="Escolha...">
+                            <option value="">Comprar</option>
+                            <option value="">Alugar</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-3">
+                        <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                        <select name="search" id="search" class="selectpicker" title="Escolha...">
+                            <option value="">Comprar</option>
+                            <option value="">Alugar</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-3">
+                        <label for="search" class="mb-2"><b>Comprar ou Alugar?</b></label>
+                        <select name="search" id="search" class="selectpicker" title="Escolha...">
+                            <option value="">Comprar</option>
+                            <option value="">Alugar</option>
+                        </select>
+                    </div>
+
+                </div>
+
+            </form>
+
+            <div class="col-6 mt-3">
+                <a href="#" class="text-front">Filtro &darr;</a>
+            </div>
+
+            <div class="col-6 mt-3 text-right">
+                <button href="#" class="btn btn-front icon-search">Pesquisar</button>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<section class="main_list_group py-5 bg-light">
+    <div class="container">
+        <div class="p-4 border-bottom border-front">
+            <h1 class="text-center">Ambiente no seu <span class="text-front"><b>estilo</b></span></h1>
+            <p class="text-center text-muted h4">Encontre um imóvel com a experiência que vc quer viver</p>
+        </div>
+
+        <div class="main_list_group_items mt-5 d-flex justify_content_around row">
+
+            <article class="main_list_group_items_item col-4 mb-4">
+                <a href="#">
+                    <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/cobertura_oto_1.jpg') no-repeat; background-size: cover;">
+                        <h2>Cobertura</h2>
+                    </div>
+                </a>
+            </article>
+
+            <article class="main_list_group_items_item col-4 mb-4">
+                <a href="#">
+                    <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/alto_padrao_1.jpg') no-repeat; background-size: cover;">
+                        <h2>Alto Padrao</h2>
+                    </div>
+                </a>
+            </article>
+
+            <article class="main_list_group_items_item col-4 mb-4">
+                <a href="#">
+                    <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/de_frente_pro_mar_original.jpg') no-repeat; background-size: cover;">
+                        <h2>De frente p mar</h2>
+                    </div>
+                </a>
+            </article>
+
+            <article class="main_list_group_items_item col-4 mb-4">
+                <a href="#">
+                    <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/compacto_1.jpg') no-repeat; background-size: cover;">
+                        <h2>Condominio</h2>
+                    </div>
+                </a>
+            </article>
+
+            <article class="main_list_group_items_item col-4 mb-4">
+                <a href="#">
+                    <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/cobertura_oto_1.jpg') no-repeat; background-size: cover;">
+                        <h2>fechado</h2>
+                    </div>
+                </a>
+            </article>
+
+            <article class="main_list_group_items_item col-4 mb-4">
+                <a href="#">
+                    <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/sala_comercial_original.jpg') no-repeat; background-size: cover;">
+                        <h2>Lojas e Salas</h2>
+                    </div>
+                </a>
+            </article>
+        </div>
+
+    </div>
+</section>
+
+<section class="main_properties py-5">
+
+    <div class="container">
+
+        <div class="mb-5 d-flex justify-content-between align-items-center border-bottom border-front">
+            <h1 class="text-front">À venda</h1>
+            <a href="#" class="text-front">Ver mais</a>
+        </div>
+
+        <div class="row">
+
+            <article class="col-4">
+
+                <div class="card main_properties_item">
+
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/1/5a3571ab-4d76-466f-8246-eff8cb98cedd.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no Rio Tavares com vista pro mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+
+                    <div class="card-footer text-muted d-flex main_properties_item_features text-center">
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/bed.png" class="img-fluid">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/garage.png" class="img-fluid">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid">
+                            <p class="text-muted">180m²</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+            <article class="col-4">
+
+                <div class="card main_properties_item">
+
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/2/a56fc386-3790-4e98-a72c-3b09d24adecd.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Casa com 3 quartos no Campeche com ótima vista</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Casa - Coqueiros <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 350.000,00</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+
+                    <div class="card-footer text-muted d-flex main_properties_item_features text-center">
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/bed.png" class="img-fluid">
+                            <p class="text-muted">2</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/garage.png" class="img-fluid">
+                            <p class="text-muted">3</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid">
+                            <p class="text-muted">80m²</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+            <article class="col-4">
+
+                <div class="card main_properties_item">
+
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/3/1c7275a0-c84e-4f25-9247-9b56664eb294.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Apartamento no prédio XPTO com vista para a Lagoa</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 600.000,00</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+
+                    <div class="card-footer text-muted d-flex main_properties_item_features text-center">
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/bed.png" class="img-fluid">
+                            <p class="text-muted">3</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/garage.png" class="img-fluid">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid">
+                            <p class="text-muted">200m²</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+        </div>
+
+    </div>
+</section>
+
+<section class="main_properties py-5 bg-light">
+
+    <div class="container">
+
+        <div class="mb-5 d-flex justify-content-between align-items-center border-bottom border-front">
+            <h1 class="text-front">Para Alugar</h1>
+            <a href="#" class="text-front">Ver mais</a>
+        </div>
+
+        <div class="row">
+
+            <article class="col-4">
+
+                <div class="card main_properties_item">
+
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/4/3d656134-3760-4c9a-af1a-503301acc0be.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no Rio Tavares com vista para o mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 2.300,00/mês</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+
+                    <div class="card-footer text-muted d-flex main_properties_item_features text-center">
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/bed.png" class="img-fluid">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/garage.png" class="img-fluid">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid">
+                            <p class="text-muted">180m²</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+            <article class="col-4">
+
+                <div class="card main_properties_item">
+
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/5/0ed8b84d-67c1-4688-86de-433ec3a498d5.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Casa com 3 quartos no Campeche com ótima vista</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Casa - Coqueiros <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 1.800,00/mês</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+
+                    <div class="card-footer text-muted d-flex main_properties_item_features text-center">
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/bed.png" class="img-fluid">
+                            <p class="text-muted">2</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/garage.png" class="img-fluid">
+                            <p class="text-muted">3</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid">
+                            <p class="text-muted">80m²</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+            <article class="col-4">
+
+                <div class="card main_properties_item">
+
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/5/8c4c5cf4-f6c0-4e0a-93fb-2e0094086845.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Apartamento no prédio XPTO com vista para a Lagoa</a></h2>
+                        <p class="main_properties_item_category">Comécio/Industrial</p>
+                        <p class="main_properties_item_type">Sala Comercial - Itacorubi<i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 4.000,00/mês</p>
+                        <a href="#" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+
+                    <div class="card-footer text-muted d-flex main_properties_item_features text-center">
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/bed.png" class="img-fluid">
+                            <p class="text-muted">3</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/garage.png" class="img-fluid">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4">
+                            <img src="assets/images/icons/util-area.png" class="img-fluid">
+                            <p class="text-muted">200m²</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+        </div>
+
+    </div>
+</section>
